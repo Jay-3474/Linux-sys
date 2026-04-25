f19fd03ab81249b3774570b48a9c922aa4d1f226
Jay Chitroda <jaychitroda95@gmail.com>
2026-04-25 13:30:56 +0530

    Add main function to output greetings and calculations

---
+++ crlib/main.cpp
@@ -0,0 +1,11 @@
+#include "main.hpp"
+
+using namespace std;
+
+int main()
+{
+	std::cout << "Hello world" << std::endl;
+	std::cout << "sum=" << sum_function(2,3) << std::endl;
+	std::cout << "mul=" << mul_function(2,3) << std::endl;
+	return 0;
+}
